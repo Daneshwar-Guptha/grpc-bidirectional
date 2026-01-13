5547ee62f91fc0fe602252470b432b53c5041592
Daneshwar <saiguptha663@gmail.com>
2026-01-13 05:23:41 +0000

    working on go language

---
+++ go-practice/practice1/practice1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt";
+
+
+func main  ()  {
+	var name1 = "guptha"
+	var name2 string
+	fmt.Println(name1);
+	fmt .Println(&name1)
+	fmt.Println("hello world");
+	fmt.Scan(name2)
+	fmt.Println(name2);
+}
